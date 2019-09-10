3ae7d870948bb7c5a0a474da31296489bbc48023
Sergey Kononenko <hackfeed@yandex.ru>
2019-09-10 08:37:12 +0300

    Fix some codestyle issues

--- lab_01/normalization.h
+++ lab_01/normalization.h
@@ -4,32 +4,33 @@ Normalization means .xxxxxxE+-yyyy representation.
 
 Input data:
 * num_t *const structed_num - number to be normalized.
-
-Output data:
-Return code - OK.
 */
-int normalize_structed_mantissa(num_t *const structed_num)
+void normalize_structed_mantissa(num_t *const structed_num)
 {
     if (structed_num->dot_position == -2)
     {
         short int mantissa_len = strlen(structed_num->mantissa_part);
         structed_num->order_int += mantissa_len;
 
-        for (short int i = mantissa_len; i > 0; --i)
+        for (short int letter_ind = mantissa_len;
+             letter_ind > 0;
+             --letter_ind)
         {
-            structed_num->mantissa_part[i] = structed_num->mantissa_part[i - 1];
+            structed_num->mantissa_part[letter_ind] =
+                structed_num->mantissa_part[letter_ind - 1];
         }
 
         structed_num->mantissa_part[mantissa_len + 1] = '\0';
     }
 
-    for (short int i = structed_num->dot_position; i > 0; --i)
+    for (short int letter_ind = structed_num->dot_position;
+         letter_ind > 0;
+         --letter_ind)
     {
-        structed_num->mantissa_part[i] = structed_num->mantissa_part[i - 1];
+        structed_num->mantissa_part[letter_ind] =
+            structed_num->mantissa_part[letter_ind - 1];
     }
 
     structed_num->mantissa_part[0] = '.';
     structed_num->order_int += structed_num->dot_position;
-
-    return OK;
 }
