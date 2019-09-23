c529f603e878c447f57546a7244294d26f86f01d
Sergey Kononenko <hackfeed@yandex.ru>
2019-09-24 00:42:54 +0300

    Add remove_leading_zeros function

--- lab_01/normalization.h
+++ lab_01/normalization.h
@@ -25,6 +25,37 @@ void base_mantissa_normalization(num_t *const structed_num)
 }
 
 /*
+Remove useless leading zeros in structed number.
+
+Input data:
+* num_t *const structed_num - number to be normalised.
+*/
+void remove_leading_zeros(num_t *const structed_num)
+{
+    short int num_len = strlen(structed_num->mantissa_part);
+
+    short int cur_digit = 0;
+
+    while (structed_num->mantissa_part[cur_digit] == '0')
+    {
+        cur_digit++;
+    }
+
+    for (short int repeats = 0; repeats < cur_digit; ++repeats)
+    {
+        for (short int letter_ind = 0; letter_ind < num_len - 1; ++letter_ind)
+        {
+            char temp = structed_num->mantissa_part[letter_ind];
+            structed_num->mantissa_part[letter_ind] =
+                structed_num->mantissa_part[letter_ind + 1];
+            structed_num->mantissa_part[letter_ind + 1] = temp;
+        }
+    }
+
+    structed_num->mantissa_part[num_len - cur_digit] = '\0';
+}
+
+/*
 Presubtraction normalization. Get both of numbers have same lenghth.
 Normalization means xxxxxxE+-yyyy representation.
 
@@ -33,6 +64,9 @@ Input data:
 */
 void presub_mantissa_normalization(num_t *const divided_num, num_t *const divider_num)
 {
+    remove_leading_zeros(divided_num);
+    remove_leading_zeros(divider_num);
+
     short int divided_num_len = strlen(divided_num->mantissa_part);
     short int divider_num_len = strlen(divider_num->mantissa_part);
 
