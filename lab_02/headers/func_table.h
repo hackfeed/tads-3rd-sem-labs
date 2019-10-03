172c1c05629d3d987032e3162aec8ceb87fe4a15
Sergey Kononenko <hackfeed@yandex.ru>
2019-10-04 01:45:34 +0300

    Update parse_table return type

--- lab_02/headers/func_table.h
+++ lab_02/headers/func_table.h
@@ -9,7 +9,7 @@ Table handling module's headers.
 
 char *get_csv_field(const char *const line, int line_num);
 
-void parse_table(const char *const filename, aio_table_t *const table);
+int parse_table(const char *const filename, aio_table_t *const table);
 
 int add_record(aio_table_t *const table);
 
