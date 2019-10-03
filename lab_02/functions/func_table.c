5f50a2be0d7a56a9ebf3482d0183bcb62ab49dc2
Sergey Kononenko <hackfeed@yandex.ru>
2019-10-03 14:16:27 +0300

    Implement table pasing branching

--- lab_02/functions/func_table.c
+++ lab_02/functions/func_table.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 
 #include "../headers/data_structures.h"
 
 #define OK 0
 #define FILE_NOT_EXIST_ERROR 3
 #define EMPTY_FILE_ERROR 4
+
+#define LINE_LEN 1024
 
 char *get_csv_field(const char *const line, int line_num)
 {
@@ -34,5 +38,28 @@ int parse_table(const char *const filename, aio_table_t *const table)
     {
         fseek(table_file, 0, SEEK_SET);
         return EMPTY_FILE_ERROR;
+    }
+
+    for (int record = 0; record < table->size_of_table; ++record)
+    {
+        char line[LINE_LEN];
+        fgets(line, LINE_LEN, table_file);
+
+        char *tmp = strdup(line);
+        srtcpy(table->main_table[record].author_last_name, get_csv_field(tmp, 0));
+        tmp = strdup(line);
+        srtcpy(table->main_table[record].book_name, get_csv_field(tmp, 1));
+        tmp = strdup(line);
+        srtcpy(table->main_table[record].publisher, get_csv_field(tmp, 2));
+        tmp = strdup(line);
+        table->main_table[record].page_count = atoi(get_csv_field(tmp, 3));
+        tmp = strdup(line);
+        table->main_table[record].book_type = atoi(get_csv_field(tmp, 4));
+
+        if (table->main_table[record].book_type == technical)
+        {
+            tmp = strdup(line);
+            strcpy(table->main_table[record].variative_part.technical_book.field, get_csv_field(tmp, 5));
+        }
     }
 }
